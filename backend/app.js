3ba874725905f63c00498f9ec240c2d595e9d85d
Rishi yadav <155768790+rishiyadav11@users.noreply.github.com>
2024-08-16 21:28:47 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: 'http://localhost:5173', // Replace with your frontend URL
+    origin: 'https://khatabook-teal.vercel.app/', // Replace with your frontend URL
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allow necessary HTTP methods
 
